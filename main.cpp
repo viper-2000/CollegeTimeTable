5097d4a9e8c83537c9133d9bf89936b109a69c4d
Tanmay <minnu.misra@gmail.com>
2021-11-15 17:45:49 +0530

    Add features to Student and Lecture Class

--- main.cpp
+++ main.cpp
@@ -18,11 +18,17 @@ class Lecture_Class : protected Course{
     int total_enrollment;
     int current_enrollment;
     int time_slot;
-    vector<string> days;
+    string days;
     int duration;
     public:
         Lecture_Class() = default;
-        Lecture_Class(int sn, string instr, int te, int ts, vector<string> d, int dr, string cc, string cn, string icn, string dept) : Course(cc, cn, icn, dept), section_number{sn}, Instructor{instr}, total_enrollment{te}, current_enrollment{0}, time_slot{ts}, days{d}, duration{dr} {}
+        Lecture_Class(int sn, string instr, int te, int ts, string d, int dr, string cc, string cn, string icn, string dept) : Course(cc, cn, icn, dept), section_number{sn}, Instructor{instr}, total_enrollment{te}, current_enrollment{0}, time_slot{ts}, days{d}, duration{dr} {}
+        void Insert_Class();
+        friend ostream& operator<< (ostream &out, Lecture_Class const& data){
+            out << "Course Code->"<< data.course_code << "\nCourse Name->"<<data.course_name<<"\nIC Name->"<< data.IC_name <<"\nDept->"<<data.department_name;
+            out << "\nSection no.->"<< data.section_number<<"\nInstructor Name->"<<data.Instructor<<"\nTotal Enrollment->"<<data.total_enrollment<<"\nCurrent Enrollment->"<<data.current_enrollment<<"\nTime Slot->"<<data.time_slot;
+            return out;
+        }
 };
 
 class Student {
@@ -44,7 +50,73 @@ class Student {
         bool register_Class(){
 
         }
+        friend ostream& operator<< (ostream &out, Student const& data) {
+            out << data.name << "-->" << data.id;
+            // and so on... 
+            return out;
+        }
 };
 int main(){
+    vector<Lecture_Class> admin_classes;
+    vector<Student> students;
+    while(true){
+        int choice;
+        cout << "Input 1 for Admin, 0 for student, -1 to exit" << endl;
+        cin >> choice;
+        if(choice == 1){
+            // Admin
+            int x;
+            cout << "Input Choice of task:\n" << "0: Add Student\n1: Add Classes\n2: Display Students\n3: Display Classes" << endl;
+            cin >> x;
+            switch (x) {
+                case 0:{
+                    string name, id;
+                    cout << "Enter Student FirstName, SecondName and ID\n";
+                    string temp;
+                    cin >> name;
+                    cin >> temp;
+                    name += temp;
+                    cin >> id;
+                    Student s{name, id};
+                    students.push_back(s);
+                    break;
+                }
+                case 1:{
+                    string cc,cn,icn,deptn,in,d;
+                    cout << "Input Class Details in order(Course Code, Course Name, IC Name, Department Name, Intrsuctor Name, Days)"<<endl;
+                    cin >> cc>>cn>>icn>>deptn>>in>>d;
+                    cout << "Enter section number, total enrollment, time slot, duration" << endl;
+                    int sn, te, ts, dur;
+                    cin >> sn>>te>>ts>>dur;
+                    Lecture_Class c{sn, in, te, ts, d, dur, cc, cn, icn, deptn};
+                    admin_classes.push_back(c);
+                    break;
+                }
+                case 2:
+                    for(auto i : students){
+                        cout << i << endl << "----------------------------" << endl;
+                    }
+                    break;
+                case 3:
+                    for(auto i : admin_classes){
+                        cout << i << endl << "-----------------------------" << endl;
+                    }
+                    break;
+                
+                default:
+                    cout << "Invalid Choice" << endl;
+                    break;
+            }
+        }
+        else if(choice == 0){
+            // Student
+        }
+        else if(choice == -1){
+            break;
+        }
+        else{
+            cout << "Invalid Choice" << endl;
+        }
+    }
     return 0;
 }
